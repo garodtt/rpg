9a1bc6dfe900a7073a2b817930a86c88035bc1d3
Vinicius Dias Campos <108832602+garodtt@users.noreply.github.com>
2025-12-16 20:17:18 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -284,7 +284,14 @@ function addHabilidade(){
     const li=document.createElement("li");
     li.dataset.nome = n;
     li.dataset.desc = d;
-    li.innerHTML = `<strong>${n}</strong> — ${d}`;
+    li.innerHTML = `
+    <strong>${n}</strong> — ${d}
+    <button class="removerHabilidade">🗑️</button>
+    `;
+    li.querySelector(".removerHabilidade").onclick = () => {
+    if(confirm(`Remover a habilidade "${n}"?`)) li.remove();
+};
+
     document.getElementById("listaHabilidades").appendChild(li);
     document.getElementById("habilidadeNome").value="";
     document.getElementById("habilidadeDesc").value="";
