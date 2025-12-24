c9bde27e5d3d364c55e41a679c2bb32cdbae4969
Vinicius Dias Campos <108832602+garodtt@users.noreply.github.com>
2025-12-24 12:36:15 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const SUPA_URL = "https://oafqjrzbkgvntwlekmlq.supabase.co";
 const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hZnFqcnpia2d2bnR3bGVrbWxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNDQ0NzYsImV4cCI6MjA4MDkyMDQ3Nn0.OPw0x8cpTRgp4IoC42mpU9H1Ld9K2cXGjBAJffAVX3I";
 
 const supabase = window.supabase.createClient(SUPA_URL, SUPA_KEY);
+console.log("✅ Supabase conectado");
+
 
 if (!supabase) {
     console.error("❌ Supabase client não encontrado.");
